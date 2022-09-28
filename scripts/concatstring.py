d6a9b03d9012245d1395518a9ebe49d433530a5c
King <chaos.feature@gmail.com>
2022-09-28 10:16:06 +0300

    Fixed code according to pycodestyle

--- scripts/concatstring.py
+++ scripts/concatstring.py
@@ -21,7 +21,20 @@ def with_comprehension(times: int = 50) -> str:
     return ''.join(str(i) for i in range(times))
 
 
-if __name__=='__main__':
-    print('concat: ', timeit.timeit('concat(5000)', globals=globals(), number=1000))
-    print('with_join: ', timeit.timeit('with_join(5000)', globals=globals(), number=1000))
-    print('with_comprehension: ', timeit.timeit('with_comprehension(5000)', globals=globals(), number=1000))
+if __name__ == '__main__':
+    print(
+        'concat: ',
+        timeit.timeit('concat(5000)', globals=globals(), number=1000)
+    )
+    print(
+        'with_join: ',
+        timeit.timeit('with_join(5000)', globals=globals(), number=1000)
+    )
+    print(
+        'with_comprehension: ',
+        timeit.timeit(
+            'with_comprehension(5000)',
+            globals=globals(),
+            number=1000
+        )
+    )
